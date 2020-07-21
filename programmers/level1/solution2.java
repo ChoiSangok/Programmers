e29fa69a79b8d35f6a8363d35e8220275077e02b
SANGOK <tkddhr12345@naver.com>
2020-07-21 21:33:51 +0900

    문자열 내 마음대로 정렬하기

---
+++ programmers/level1/solution2.java
@@ -0,0 +1,56 @@
+package programmers;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+class Solution {
+    public String[] solution(String[] answer, int n) {
+    	
+        String[] str = new String[answer.length];
+        
+        for(int i=0;i<answer.length;i++) {
+        	str[i] = answer[i].charAt(n)+answer[i];
+        }
+        
+        Arrays.sort(str);
+        
+		for(int i=0;i<answer.length;i++) {
+			str[i].substring(1);
+		}
+        return str;
+    }
+		
+    
+
+	
+	public static void main(String[] args) {
+
+		
+		String[] answer = 	{"sun", "bed", "car"};
+		String[] str = new String[answer.length];
+		
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("몇번째 인덱스"); 
+		int n=sc.nextInt();
+
+		char ch = 0;
+		
+		
+		for(int i=0;i<answer.length;i++) {
+
+			str[i] = answer[i].charAt(n)+answer[i];
+
+		}
+		Arrays.sort(str);
+		
+		for(int i=0;i<answer.length;i++) {
+			System.out.println(str[i].substring(1));
+		}
+		
+		
+		
+	
+	}
+}

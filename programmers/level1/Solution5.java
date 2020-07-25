6506d430492492e413578578aa3b7a551dc0e9d9
SANGOK <tkddhr12345@naver.com>
2020-07-25 15:12:06 +0900

    문자열 내림차순으로 배열하기

---
+++ programmers/level1/Solution5.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String s) {
+    	String answer = "";
+    	String[] str = new String[s.length()];
+
+    	str=s.split("");      
+    	Arrays.sort(str);
+    	for (int i=str.length-1;i>=0;i--){
+    		answer+=str[i];
+    	}
+        return answer;
+    }
+}

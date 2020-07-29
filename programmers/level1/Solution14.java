efd053de30017608e780f4505e1db825fc10ac36
SANGOK <tkddhr12345@naver.com>
2020-07-29 21:43:27 +0900

    문자열을 정수로 바꾸기

---
+++ programmers/level1/Solution14.java
@@ -0,0 +1,15 @@
+public class Solution {
+	   
+    public static int solution(String s) {
+
+    	int answer=0;
+     	
+    	answer = Integer.parseInt(s);
+        return answer;
+    }
+    public static void main(String[] args) {
+    	String x="-1234";
+
+    	System.out.println(solution(x));
+    }
+}

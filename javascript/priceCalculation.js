324db773f9ffe09937ec9ec343d5b835cff2dedd
ChoiSangOk <tkddhr12345@naver.com>
2022-05-04 16:17:32 +0900

    [javascript] 부족한 금액 계산하기

--- javascript/priceCalculation.js
+++ javascript/priceCalculation.js
@@ -1,5 +1,15 @@
 function solution(price, money, count) {
-    var answer = -1;
 
-    return answer;
+    let total = 0;
+
+    for (let i = 0; i < count; i++) {
+        total += price + (price * i)
+    }
+    // 모자란돈 
+    let answer = money - total;
+
+    if (money < total) return Math.abs(answer)
+    else return 0;
 }
+
+solution(1, 20, 4)

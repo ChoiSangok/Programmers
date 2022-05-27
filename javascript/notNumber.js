39a7dbbe21e137a0af301cdc6bc804cab02b9d8d
ChoiSangOk <tkddhr12345@naver.com>
2022-05-27 16:22:11 +0900

    [문제푸는중]

--- javascript/notNumber.js
+++ javascript/notNumber.js
@@ -1,28 +1,44 @@
-function solution(numbers) {
 
-    let answer = 45;
-    let totalarr = 0;
-    
-    numbers.forEach(el => {
-        totalarr += el;
-    });
-
-    return answer - totalarr;
+function solution(s) {
+    console.log(s)
+    var answer = 0;
+    return answer;
 }
 
+solution(one4seveneight)
 
-let numbers = [1, 2, 3, 4, 6, 7, 8, 0];
-
-solution(numbers)
-// numbers	result
-// [1, 2, 3, 4, 6, 7, 8, 0]	14
-// [5, 8, 4, 0, 6, 7, 9]	6
-
-
+// 숫자	영단어
+// 0	zero
+// 1	one
+// 2	two
+// 3	three
+// 4	four
+// 5	five
+// 6	six
+// 7	seven
+// 8	eight
+// 9	nine
+// 제한사항
+// 1 ≤ s의 길이 ≤ 50
+// s가 "zero" 또는 "0"으로 시작하는 경우는 주어지지 않습니다.
+// return 값이 1 이상 2, 000, 000, 000 이하의 정수가 되는 올바른 입력만 s로 주어집니다.
+// 입출력 예
+// s	result
+// "one4seveneight"	1478
+// "23four5six7"	234567
+// "2three45sixseven"	234567
+// "123"	123
 // 입출력 예 설명
 // 입출력 예 #1
 
-// 5, 9가 numbers에 없으므로, 5 + 9 = 14를 return 해야 합니다.
+// 문제 예시와 같습니다.
 // 입출력 예 #2
 
-// 1, 2, 3이 numbers에 없으므로, 1 + 2 + 3 = 6을 return 해야 합니다.
+// 문제 예시와 같습니다.
+// 입출력 예 #3
+
+// "three"는 3, "six"는 6, "seven"은 7에 대응되기 때문에 정답은 입출력 예 #2와 같은 234567이 됩니다.
+// 입출력 예 #2와 #3과 같이 같은 정답을 가리키는 문자열이 여러 가지가 나올 수 있습니다.
+// 입출력 예 #4
+
+// s에는 영단어로 바뀐 부분이 없습니다.

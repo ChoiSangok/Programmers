89e5eab5c6ef5dc02dcad477ab852c083f5c9b7c
ChoiSangOk <tkddhr12345@naver.com>
2022-05-31 09:24:51 +0900

    소수찾기

---
+++ javascript/findDecimal.js
@@ -0,0 +1,20 @@
+function solution(n) {
+    var answer = 0;
+    let arr = [];
+    for( let i=2; i<=n; i++){
+        arr[i] = i;
+    }
+    for( let i =2; i<=n; i++){
+        for( let j = i+i; j<=n; j += i){
+            arr[j] = 0;
+        }
+    }
+    for ( let i=2; i<=n; i++){
+        if( arr[i] !== 0 ){
+            answer ++;
+        }
+    }
+
+    return answer;
+}
+console.log(solution(10));

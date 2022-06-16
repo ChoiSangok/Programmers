5faef1b4cfb40d505ee0d2ac1a5df503c36b727f
ChoiSangOk <tkddhr12345@naver.com>
2022-06-16 15:00:13 +0900

    새로운아이디생성

--- javascript/newId.js
+++ javascript/newId.js
@@ -1,7 +1,10 @@
 
 function solution(new_id) {
     
-    let answer = new_id.toLowerCase().replace(/[^\w-._]+/g, '').replace(/\.{2,}/g, '.').replace(/(^\.)|(\.$)/g, "");
+    let answer = new_id.toLowerCase()
+                    .replace(/[^\w-._]+/g, '')
+                    .replace(/\.{2,}/g, '.')
+                    .replace(/(^\.)|(\.$)/g, "");
     if (!answer) answer = "a"
 
     answer = answer.length > 15 ? answer.substr(0, 15).replace(/\.$/, '') : answer
@@ -12,6 +15,6 @@ function solution(new_id) {
     
 }
 
-solution("...!@BaT#*..y.abcdefghijklm.")
-// solution("123_.def")
+// solution("...!@BaT#*..y.abcdefghijklm.")
+solution("123_.def")
 // solution("=.=")

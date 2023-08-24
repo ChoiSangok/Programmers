3b076772a10af54a65f3a3efd0b8fccc2e68e1cd
SANGOK <tkddhr12345@naver.com>
2023-08-24 21:53:29 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 2.44 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -0,0 +1,13 @@
+function solution(t, p) {
+
+    let count = 0;
+
+    for (let i = 0; i <= t.length - p.length; i++) {
+        if (parseInt(t.substring(i, i + p.length)) <= parseInt(p)) {
+            count++;
+        }
+    }
+    console.log(count);
+
+    return count;
+}
